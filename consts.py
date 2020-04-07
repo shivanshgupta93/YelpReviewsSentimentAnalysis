7c9dfeba55353357665c5b2cedb62db0deeb6f35
Shivansh Gupta <55259717+shivanshgupta93@users.noreply.github.com>
2020-04-07 01:15:35 -0400

    Updated the dashboard

--- consts.py
+++ consts.py
@@ -1,3 +1,3 @@
-YELP_API_KEY = "ooZNDIGi9vNX4jeXrGd75IWxfzCwOO213GhFGnupa7yTULBAmkaiPlbzNe7Bc96xglJpEeMuqzznhf2JBMsyNOUNLIbM06spYdIEOKChTcpLswACCYxC_-t52_6IXnYx"
+YELP_API_KEY = ""
 
 URL = "https://api.yelp.com/v3/businesses/search"
